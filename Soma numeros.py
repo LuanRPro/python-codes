771b7a4f5cb92df2cb572ab18365eda7dce3711d
LuanRPro <luanreispro2@gmail.com>
2022-07-08 15:26:36 -0300

    primeiro commit do repositório

---
+++ Soma numeros.py
@@ -0,0 +1,5 @@
+primeiroNumero = float(input('Digite o primeiro número:'))
+segundoNumero = float(input('Digite o segundo número:'))
+soma = primeiroNumero + segundoNumero
+
+print('A soma dos números {} e {} é {}'.format(primeiroNumero, segundoNumero, soma))
